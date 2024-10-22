5ea9b1d69640be0073688c609bad44c998916f5b
DesignerDigidoers <designer@digidoers.in>
2024-10-22 11:06:17 +0530

    feat: add support for connected repositories to show in dashboard #15

--- resources/views/components/settings/github.blade.php
+++ resources/views/components/settings/github.blade.php
@@ -49,13 +49,7 @@
                     </span>
                 </button>
                 <div x-show="accountsContainerOpen" @click.away="accountsContainerOpen = false"
-<<<<<<< ours
-                     class="absolute z-10 mt-1 w-full bg-white dark:bg-gray-800 shadow-lg max-h-60 rounded-md py-1 text-base ring-1 ring-black ring-opacity-5 overflow-auto focus:outline-none sm:text-sm">
-||||||| ancestor
-                     class="absolute z-10 mt-1 w-full bg-white shadow-lg max-h-60 rounded-md py-1 text-base ring-1 ring-black ring-opacity-5 overflow-auto focus:outline-none sm:text-sm">
-=======
                      class="absolute z-10 mt-1 w-full bg-white dark:bg-gray-800 shadow-lg max-h-60 rounded-md py-1 text-base ring-1 ring-black ring-opacity-5 overflow-auto focus:outline-none sm:text-sm dark:text-white">
->>>>>>> theirs
                     <ul tabindex="-1" role="listbox" aria-labelledby="listbox-label"
                         aria-activedescendant="listbox-option-3">
                         <template x-for="(name, id) in githubAccounts" :key="id">
@@ -80,23 +74,6 @@
             <div class="relative" x-ref="dropdownContainer">
                 <div class="flex">
                     <input
-<<<<<<< ours
-                            x-model="searchTerm"
-                            @input="filterRepositories(); repoOpen = true"
-                            @focus="repoOpen = true"
-                            @click="repoOpen = true"
-                            type="text"
-                            class="w-full border border-gray-300 dark:border-gray-700 bg-gray-100 dark:bg-gray-800 rounded-md shadow-sm pl-3 pr-10 py-2 text-sm focus:outline-none focus:ring-1 focus:ring-gray-500 focus:border-gray-500 dark:text-white"
-                            placeholder="Search repositories...">
-||||||| ancestor
-                        x-model="searchTerm"
-                        @input="filterRepositories(); repoOpen = true"
-                        @focus="repoOpen = true"
-                        @click="repoOpen = true"
-                        type="text"
-                        class="w-full border border-gray-300 rounded-md shadow-sm pl-3 pr-10 py-2 text-sm focus:outline-none focus:ring-1 focus:ring-gray-500 focus:border-gray-500"
-                        placeholder="Search repositories...">
-=======
                         x-model="searchTerm"
                         @input="filterRepositories(); repoOpen = true"
                         @focus="repoOpen = true"
@@ -104,24 +81,10 @@
                         type="text"
                         class="w-full border border-gray-300 dark:border-gray-700 bg-gray-100 dark:bg-gray-800 rounded-md shadow-sm pl-3 pr-10 py-2 text-sm focus:outline-none focus:ring-1 focus:ring-gray-500 focus:border-gray-500 dark:text-white"
                         placeholder="Search repositories...">
->>>>>>> theirs
                     <button @click="repoOpen = !repoOpen" type="button"
-<<<<<<< ours
-                            class="ml-2 border border-gray-300 dark:border-gray-700 bg-gray-100 dark:bg-gray-800 rounded-md shadow-sm px-3 py-2 text-sm focus:outline-none focus:ring-1 focus:ring-gray-500 focus:border-gray-500">
-                        <svg class="h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
-                             fill="currentColor">
-                            <path fill-rule="evenodd"
-                                  d="M10 3a1 1 0 01.707.293l3 3a1 1 0 01-1.414 1.414L10 5.414 7.707 7.707a1 1 0 01-1.414-1.414l3-3A1 1 0 0110 3zm-3.707 9.293a1 1 0 011.414 0L10 14.586l2.293-2.293a1 1 0 011.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z"
-                                  clip-rule="evenodd"/>
-||||||| ancestor
-                            class="ml-2 bg-white border border-gray-300 rounded-md shadow-sm px-3 py-2 text-sm focus:outline-none focus:ring-1 focus:ring-gray-500 focus:border-gray-500">
-                        <svg class="h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-                            <path fill-rule="evenodd" d="M10 3a1 1 0 01.707.293l3 3a1 1 0 01-1.414 1.414L10 5.414 7.707 7.707a1 1 0 01-1.414-1.414l3-3A1 1 0 0110 3zm-3.707 9.293a1 1 0 011.414 0L10 14.586l2.293-2.293a1 1 0 011.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z" clip-rule="evenodd"/>
-=======
                             class="ml-2 border border-gray-300 dark:border-gray-700 bg-gray-100 dark:bg-gray-800 rounded-md shadow-sm px-3 py-2 text-sm focus:outline-none focus:ring-1 focus:ring-gray-500 focus:border-gray-500">
                         <svg class="h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
                             <path fill-rule="evenodd" d="M10 3a1 1 0 01.707.293l3 3a1 1 0 01-1.414 1.414L10 5.414 7.707 7.707a1 1 0 01-1.414-1.414l3-3A1 1 0 0110 3zm-3.707 9.293a1 1 0 011.414 0L10 14.586l2.293-2.293a1 1 0 011.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z" clip-rule="evenodd"/>
->>>>>>> theirs
                         </svg>
                     </button>
                 </div>
